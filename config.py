37dc9f12b1c950ac33bef3a460b32857d927fb2d
Seyed <seyed.aman.zargari@gmail.com>
2024-09-28 13:57:03 +0330

    add openweather api key and url

--- config.py
+++ config.py
@@ -1,3 +1,9 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+OpenWeatherMap_API_KEY = os.environ.get('OPENWEATHERMAP_API_KEY')
+OpenWeatherMap_API_URL = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}".format(api_key=OpenWeatherMap_API_KEY, lat='{lat}', lon='{lon}')
