f07c736cf8c47cb6c60b588acde0ff3484d518f9
Seyed <seyed.aman.zargari@gmail.com>
2024-09-28 12:10:07 +0330

    add use camera module

---
+++ camera.py
@@ -0,0 +1,42 @@
+import cv2
+import numpy as np
+
+class Camera:
+    def __init__(self, camera_id, rgb=True, resize=None):
+        self.camera = cv2.VideoCapture(camera_id)
+        # find frame rate from camera
+        self.frame_rate = self.camera.get(cv2.CAP_PROP_FPS)
+        
+        self.size = (int(self.camera.get(cv2.CAP_PROP_FRAME_WIDTH)),
+                     int(self.camera.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+        
+        self.black_image = np.zeros((self.size[1], self.size[0], 3), np.uint8)
+        
+        self.is_rgb = rgb
+        self.resize = resize
+        
+        if self.resize:
+            self.size = self.resize
+            self.black_image = np.zeros((self.size[1], self.size[0], 3), np.uint8)
+
+    def get_frame(self):
+        success, image = self.camera.read()
+        
+        if self.resize : image = cv2.resize(image, self.size)
+        
+        if not success:
+            return self.black_image        
+        
+        if self.is_rgb:
+            return cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        else:
+            return image
+
+    def release(self):
+        self.camera.release()
+    
+    def get_size(self):
+        return self.size
+    
+    def get_framerate(self):
+        return self.frame_rate
