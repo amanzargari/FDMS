fbd8f610c8c025444f63df8e86b041e14bdbdbf2
Seyed <seyed.aman.zargari@gmail.com>
2024-09-28 14:26:45 +0330

    update weather url in config file

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ load_dotenv()
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 OpenWeatherMap_API_KEY = os.environ.get('OPENWEATHERMAP_API_KEY')
-OpenWeatherMap_API_URL = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}".format(api_key=OpenWeatherMap_API_KEY, lat='{lat}', lon='{lon}')
+OpenWeatherMap_API_URL = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}&units=metric".format(api_key=OpenWeatherMap_API_KEY, lat='{lat}', lon='{lon}')
