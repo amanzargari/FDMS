894d5caee30689c83e63b29379ad44acf6247c6e
Seyed <seyed.aman.zargari@gmail.com>
2024-10-03 01:07:37 +0330

    fully update map code

--- map.py
+++ map.py
@@ -1,23 +1,91 @@
-from folium import Map, Marker
-import folium
+import osmium
+import json
+from PyQt6.QtCore import QUrl
+from PyQt6.QtWebEngineCore import QWebEngineUrlRequestInterceptor
+import urllib.parse
+import pickle
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+import threading
 import config
-import os
 
-icon_path = os.path.join(config.ROOT_DIR, 'static', 'images', 'marker.png')
-icon = folium.features.CustomIcon(icon_path, icon_size=(25, 41))
+def start_http_server():
+    handler = SimpleHTTPRequestHandler
+    httpd = HTTPServer(('localhost', 8000), handler)
+    thread = threading.Thread(target=httpd.serve_forever)
+    thread.daemon = True
+    thread.start()
 
-def create_map(lat, lon) -> str:
-    # Create a folium map centered at the given latitude and longitude
-    # Use the 'Stamen Terrain' tiles, which can be cached for offline use
-    folium_map = Map(
-        location=[lat, lon],
-        zoom_start=15,
-        #tiles='Stamen Terrain',  # Use Stamen Terrain tiles (can be cached offline)
-        #attr='Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.'
-    )
+class OSMHandler(osmium.SimpleHandler):
+    def __init__(self, cache_file=None):
+        super(OSMHandler, self).__init__()
+        #self.nodes = []
+        self.ways = []
+        
+        if cache_file:
+            with open(config.MAP_FILE, 'rb') as f:
+                map_data = pickle.load(f)
+            #self.nodes = map_data['nodes']
+            self.ways = map_data['ways']
+            map_data = None
 
-    # Add a marker for the location 
-    Marker([lat, lon], popup="Current Location", icon=icon).add_to(folium_map)
 
-    # Return the map as HTML
-    return folium_map._repr_html_()
+class RequestInterceptor(QWebEngineUrlRequestInterceptor):
+    def __init__(self, ways):
+        super().__init__()
+        self.ways = ways
+
+    def interceptRequest(self, info):
+        if info.requestUrl().path() == "/map_data":
+            data = json.dumps(self.ways)
+            info.redirect(QUrl(f"data:application/json,{urllib.parse.quote(data)}"))
+
+def load_map(latitude, longitude):
+        html = f"""
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>GPS Map with Car Icon</title>
+            <link rel="stylesheet" href="http://localhost:8000/static/css/leaflet.css"/>
+            <script src="http://localhost:8000/static/scripts/leaflet.js"></script>
+            <script src="http://localhost:8000/static/scripts/leaflet.rotatedMarker.js"></script>
+            <style>
+                #map {{ height: 100vh; width: 100%; }}
+            </style>
+        </head>
+        <body style="margin:0; padding:0;">
+            <div id="map"></div>
+            <script>
+                const map = L.map('map').setView([{latitude}, {longitude}], 15);
+                L.tileLayer('https://{{s}}.tile.openstreetmap.org/{{z}}/{{x}}/{{y}}.png', {{
+                    attribution: '© OpenStreetMap contributors'
+                }}).addTo(map);
+
+                const customLayer = L.layerGroup().addTo(map);
+                
+                // Car icon
+                const carIcon = L.icon({{
+                    iconUrl: 'http://localhost:8000/static/images/marker.png',
+                    iconSize: [25, 41],
+                    iconAnchor: [12, 41],
+                    popupAnchor: [1, -34],
+                }});
+                const marker = L.marker([{latitude}, {longitude}], {{icon: carIcon}}).addTo(map);
+
+                fetch('/map_data')
+                    .then(response => response.json())
+                    .then(data => {{
+                        data.forEach(way => {{
+                            L.polyline(way.nodes, {{color: '#ff0000', weight: 2}}).addTo(customLayer);
+                        }});
+                    }});
+
+                window.updateMarker = function(lat, lon, speed, heading) {{
+                    marker.setLatLng([lat, lon]);
+                    marker.setRotationAngle(heading);
+                    map.panTo([lat, lon]);
+                }};
+            </script>
+        </body>
+        </html>
+        """
+        return html
